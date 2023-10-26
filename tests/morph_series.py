d626f6e1893777f086a68bf37dbafb0ab1a0d0d0
Liza <sazonova.astro@gmail.com>
2023-10-26 18:58:52 +0000

    morph series

--- tests/morph_series.py
+++ tests/morph_series.py
@@ -8,18 +8,23 @@ from scipy import stats
 from joblib import Parallel, delayed
 import multiprocessing
 import statmorph as sm
+from scipy import ndimage
 
 from astropy import units as u
 from astropy.stats import gaussian_fwhm_to_sigma
 from astropy.convolution import Gaussian2DKernel
+from photutils.segmentation import detect_sources
 
 from photutils.aperture import EllipticalAperture, CircularAnnulus, CircularAperture
 import galsim
 
 # from asymmetry import get_asymmetry
 sys.path.append('../')
-from galaxy_generator import gen_image, gen_galaxy, petrosian_sersic, create_clumps, add_source_to_image, sky_noise, petrosian_sersic
+from galaxy_generator import add_source_to_image, simulate_perfect_galaxy, sky_noise, petrosian_sersic
 from asymmetry import get_asymmetry, get_residual
+from skimage import transform as T
+from importlib import resources as impresources
+import templates
 
 num_cores = multiprocessing.cpu_count()-3
 
@@ -27,17 +32,24 @@ plt.rcParams['font.size'] = 9
 plt.rcParams['axes.xmargin'] = .05  # x margin.  See `axes.Axes.margins`
 plt.rcParams['axes.ymargin'] = .05  # y margin.  See `axes.Axes.margins`
 
-
-##### Telescope parameters
+#################### SDSS set-up ###########################
+sdss_ra = 150
+sdss_dec = 2.3
 filt = 'r'
-bandpass_file = "../passband_sdss_" + filt
-bandpass = galsim.Bandpass(bandpass_file, wave_type = u.angstrom)
+bandpass_file = "passband_sdss_" + filt
+inp_file = (impresources.files(templates) / bandpass_file)
+throughput = galsim.LookupTable.from_file(inp_file)
+bandpass = galsim.Bandpass(throughput, wave_type = u.angstrom)
 ## gain, exptime and diameter of telescope
 telescope_params = {'g':4.8, 't_exp':53.91, 'D':2.5}
 ## effective wavelength and width of filter
 transmission_params = {'eff_wav':616.5, 'del_wav':137}
+g, t_exp, D = telescope_params['g'],telescope_params['t_exp'],telescope_params['D']
+transmission = bandpass(transmission_params['eff_wav'])
+#############################################################
 
-def get_perfect_galaxy(mag, r_eff, fov_reff=10, pxscale=0.396, sersic_n=1, q=1, beta=0):
+
+def get_perfect_galaxy(mag, r_eff, fov_reff, pxscale, sersic_n=1, q=1, beta=0):
     
     sdss_ra = 150
     sdss_dec = 2.3
@@ -49,8 +61,7 @@ def get_perfect_galaxy(mag, r_eff, fov_reff=10, pxscale=0.396, sersic_n=1, q=1, 
     field_image, wcs = gen_image(sdss_ra, sdss_dec, pxscale, fov, fov)
 
     # create a galaxy with given params
-    galaxy = gen_galaxy(mag=mag, re=r_eff, n=sersic_n, q=q, beta=beta, telescope_params=telescope_params, 
-                        transmission_params=transmission_params, bandpass=bandpass)
+    galaxy = gen_galaxy(mag=mag, re=r_eff, n=sersic_n, q=q, beta=beta)
     
     # get petrosian radius of galaxy in px
     r_pet = petrosian_sersic(fov, r_eff, 1)/pxscale
@@ -121,43 +132,73 @@ def get_morphology_dict(source_morph, pxscale, prefix=""):
 
     return output
 
-def single_galaxy_run(filepath, mag, r_eff, sersic_n, q, n_clumps, sky_mag, psf_fwhm, pxscale=0.396):
+def single_galaxy_run(filepath, mag, r_eff, sersic_n, q, n_clumps, sky_mag, psf_fwhm, pxscale):
 
+#     print('here')
     ##### Generate the galaxy image
-    # Generate galaxy model
-    field, galaxy, r_pet = get_perfect_galaxy(mag, r_eff, fov_reff=20, sersic_n=sersic_n, q=q)
     # generate all the clumps and their positions
-    clumps, all_xi, all_yi = create_clumps(field, r_pet, n_clumps, mag, telescope_params, transmission_params, bandpass)
-    # noiseless, psf_free image
-    image_perfect = add_source_to_image(field, galaxy, clumps, all_xi, all_yi, psf_fwhm=0)
-    # convolve sources with psf and add to image
-    image_psf = add_source_to_image(field, galaxy, clumps, all_xi, all_yi, psf_fwhm)
-    # add sky and poisson noise
-    image_noisy, sky_flux = sky_noise(image_psf, sky_mag, pxscale, telescope_params, transmission_params, bandpass, rms_noise=True)
+    # Generate clumps
+    perfect_px = 0.168
+    rs = np.random.random(n_clumps)*(0.8-0.05)+0.05
+    fs = np.random.random(n_clumps)*(0-0.05) + 0.05
 
-    image_perfect = image_perfect.array
-    image_noisy = image_noisy.array
+    smin = 0.2
+    smax = 3
+    sigs = np.random.random(n_clumps)*(smax-smin) + smin
+    thetas = np.random.random(n_clumps)*360
+
+    fs = np.power(10, fs)
+
+    clump_properties = {
+        'r' : rs,
+        'theta' : thetas,
+        'flux' : fs,
+        'sigma' : sigs
+    }
+
+    # Generate galaxy model at two resolutions
+    try:
+        image_perfect, params_res, r_pet_res = simulate_perfect_galaxy(mag, r_eff, perfect_px, 15, sersic_n, q, 0, n_clumps, clump_properties)
+    except:
+        return
+    img, params, r_pet = simulate_perfect_galaxy(mag, r_eff, pxscale, 15, sersic_n, q, 0, n_clumps, clump_properties)
+#     print('made a galaxy')
+    
+    # Convolve with PSF
+    image_perfect = add_source_to_image(**params_res, psf_fwhm=0, pxscale=perfect_px, psf_method="astropy")
+    image_psf = add_source_to_image(**params, psf_fwhm=psf_fwhm, pxscale=pxscale, psf_method="astropy")
+    image_noisy, sky_flux = sky_noise(image_psf, sky_mag=sky_mag, pixel_scale=pxscale, rms_noise=True)
 
 
-    # Create segmap at 3 pet
+    return image_perfect, image_noisy 
+
+    # Create segmap at 2 pet
     xc = image_perfect.shape[0]/2
-    segmap = CircularAperture((xc,xc), 3*r_eff/pxscale).to_mask().to_image(image_perfect.shape)
-    segmap = np.array(segmap > 0.1, dtype=int)
-
+    segmap_res = CircularAperture((xc,xc), 1.5*r_pet_res).to_mask().to_image(image_perfect.shape)
+    segmap_res = (segmap_res > 0.1).astype(int)
+    segmap = detect_sources(image_noisy, sky_flux, 300)
+    try:
+        segmap = (ndimage.gaussian_filter(segmap.data.astype(float), 3) > 0.1).astype(int)
+    except:
+        return 
+    
     # Create psf
     psf_std = psf_fwhm * gaussian_fwhm_to_sigma
     psf = Gaussian2DKernel(x_stddev=psf_std/pxscale).array
 
     ###### Run statmorph 
-    morphs_real = sm.source_morphology(image_perfect, segmap=segmap, gain=1, psf=psf)[0]
-    morphs_noisy = sm.source_morphology(image_noisy, segmap=segmap, gain=1, psf=psf)[0]
-    out_real = get_morphology_dict(morphs_real, pxscale, prefix='_real')
+    morphs_real = sm.source_morphology(image_perfect, segmap=segmap_res, gain=1)[0]
+    try:
+        morphs_noisy = sm.source_morphology(image_noisy, segmap=segmap, gain=1, psf=psf)[0]
+    except:
+        return
+    out_real = get_morphology_dict(morphs_real, perfect_px, prefix='_real')
     out_noisy = get_morphology_dict(morphs_noisy, pxscale, prefix='_noisy')
 
     ##### Calculate SNR
-    ap_real = EllipticalAperture((xc,xc), 2*r_pet, 2*r_pet)
-    ap_sky = CircularAnnulus((xc,xc), 2*1.5*r_pet, 2*2*r_pet)
-    var = sky_flux + image_perfect
+    ap_real = EllipticalAperture((xc,xc), 1.5*r_pet_res, 1.5*r_pet_res)
+    ap_sky = CircularAnnulus((xc,xc), 2*1.5*r_pet_res, 2*2*r_pet_res)
+    var = sky_flux**2 * (perfect_px/pxscale)**2 + image_perfect
     snr = image_perfect / np.sqrt(var)
     snr_px = ap_real.do_photometry(snr)[0][0] / ap_real.do_photometry(np.ones_like(snr))[0][0]
 
@@ -165,7 +206,7 @@ def single_galaxy_run(filepath, mag, r_eff, sersic_n, q, n_clumps, sky_mag, psf_
     # imgs = [image_perfect, image_noisy]
     inputs = dict(
         mag=mag, r_eff=r_eff, r_pet=r_pet, sersic_n=sersic_n, q=q, n_clumps=n_clumps, 
-        sky_mag=sky_mag, psf=psf_fwhm, snr=snr_px
+        sky_mag=sky_mag, psf=psf_fwhm, snr=snr_px, pxscale=pxscale
     )
     output = {
         'input' : inputs,
@@ -193,11 +234,12 @@ if __name__ == '__main__':
 
     ## Range of values to try
     lims = {
-        'mag' : (10, 16),
-        'sky_mag' : (20, 26),
-        'n_clumps' : (5, 30),
-        'psf_fwhm' : (0, 2),
-        'sersic_n' : (1, 3),
+        'mag' : (12, 16),
+        'sky_mag' : (21, 27),
+        'n_clumps' : (5, 60),
+        'psf_fwhm' : (0, 3),
+        'sersic_n' : (1, 4),
+        'px' : (0.168, 0.5),
     }
 
     # Generate parameters for n galaxies
@@ -207,7 +249,11 @@ if __name__ == '__main__':
     sky_mags = stats.uniform.rvs(loc=lims['sky_mag'][0], scale=lims['sky_mag'][1] - lims['sky_mag'][0], size=N)
     n_clumps = np.random.randint(low=lims['n_clumps'][0], high=lims['n_clumps'][1], size=N)
     psfs = stats.uniform.rvs(loc=lims['psf_fwhm'][0], scale=lims['psf_fwhm'][1] - lims['psf_fwhm'][0], size=N)
+#     pxscales = stats.uniform.rvs(loc=lims['px'][0], scale=lims['px'][1] - lims['px'][0], size=N)
+#     for i in range(N):
+#         pxscales[i] = np.min([pxscales[i], psfs[i]/2])
     qs = stats.uniform.rvs(loc=0.2, scale=0.8, size=N)
+    qs[ns > 3.5] = 1.0
     rs = -1.9*mags + 35 + stats.norm.rvs(loc=0, scale=1.5, size=N)
     rs[rs <= 1] = 1
     rs[rs >= 20] = 20
@@ -215,6 +261,6 @@ if __name__ == '__main__':
     ### Run the execution in parallel
     Parallel(n_jobs=num_cores)(delayed(single_galaxy_run)(
         filepath=f'{args.path}/{i}.pkl', mag=mags[i], r_eff=rs[i], sersic_n=ns[i],
-        q=qs[i], n_clumps=n_clumps[i], sky_mag=sky_mags[i], psf_fwhm=psfs[i]
+        q=qs[i], n_clumps=n_clumps[i], sky_mag=sky_mags[i], psf_fwhm=psfs[i], pxscale=psfs[i]/2
     ) for i in tqdm(range(N), total=N) )
 
